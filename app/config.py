067f14e6c068be14bb44efa39bb8c4562cd27961
Chintan Kanal <chintankanal@example.com>
2026-05-24 15:56:55 +0530

    updated config to include database_url and other config

--- app/config.py
+++ app/config.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str
-    jwt_secret: str
+    database_url: str = "postgresql://postgres:postgres@localhost:5432/rallypoint"
+    jwt_secret: str = "tnud1zc6gad971g0irurptmie5yahlge"
     jwt_expiry_hours: int = 24
     frontend_url: str = "http://localhost:5173"
     webhook_secret: str = ""
     webhook_url: str = ""
-    internal_job_secret: str
+    internal_job_secret: str = "u9dqoepr3hawymg2b3omaq82uar0qmid"
     resend_api_key: str = "re_69RLa5xi_FvBVq7ytu98QizFLjznt9Tc1"
     from_email: str = "onboarding@resend.dev"
 
